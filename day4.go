9ce32922aab359f7ba3edc7b0d40ebea854d78d2
Jacob <jacob.angel@gmail.com>
2020-01-04 18:52:35 -0500

    Finishing day4

--- day4.go
+++ day4.go
@@ -7,8 +7,46 @@ import (
 const LOWER_BOUND = 235741
 const UPPER_BOUND = 706948
 
+func isValidGroupedPassword(guess int) bool {
+	return hasEvenDouble(guess) && isAscending((guess))
+}
+
 func isValidPassword(guess int) bool {
 	return hasDouble(guess) && isAscending(guess)
+}
+
+func hasEvenDouble(guess int) bool {
+
+	if guess < 10 {
+		return false
+	}
+
+	lastDigit := -1
+	onaRun := false
+	decremented := false
+	doubleCount := 0
+	for guess > 0 {
+		nextDigit := guess % 10
+		if nextDigit == lastDigit {
+			if onaRun {
+				if !decremented {
+					doubleCount--
+				}
+				decremented = true
+			} else {
+				doubleCount += 1
+				decremented = false
+				onaRun = true
+			}
+		} else {
+			onaRun = false
+			decremented = false
+		}
+
+		lastDigit = nextDigit
+		guess = guess / 10
+	}
+	return doubleCount != 0
 }
 
 func hasDouble(guess int) bool {
@@ -54,9 +92,25 @@ func crackTheCode() int {
 	return count
 }
 
+func crackTheCode2() int {
+	count := 0
+	for i := LOWER_BOUND + 1; i < UPPER_BOUND; i++ {
+		if isValidGroupedPassword(i) {
+			count++
+		}
+	}
+	return count
+}
+
 func day41() {
 	fmt.Println("Day 4.1")
 	code := crackTheCode()
+	fmt.Printf("The possible passwords are: '%d'\n", code)
+}
+
+func day42() {
+	fmt.Println("Day 4.2")
+	code := crackTheCode2()
 	fmt.Printf("The possible passwords are: '%d'\n", code)
 }
 
@@ -64,5 +118,6 @@ func day4() {
 	fmt.Println("Day 4")
 	fmt.Println("---------")
 	day41()
+	day42()
 	fmt.Println()
 }
--- day4_test.go
+++ day4_test.go
@@ -23,3 +23,20 @@ func TestHasDouble(t *testing.T) {
 	assert.True(t, hasDouble(12378911))
 	assert.True(t, hasDouble(1112378911))
 }
+
+func TestHasGroupedDouble(t *testing.T) {
+	assert.True(t, hasEvenDouble(11))
+	assert.False(t, hasEvenDouble(123789))
+	assert.False(t, hasEvenDouble(12))
+	assert.True(t, hasEvenDouble(112378911))
+	assert.True(t, hasEvenDouble(1112378911))
+	assert.False(t, hasEvenDouble(11123789))
+	assert.True(t, hasEvenDouble(11112378911))
+}
+
+func TestIsValidGroupedPassword(t *testing.T) {
+	assert.False(t, isValidGroupedPassword(111111))
+	assert.True(t, isValidGroupedPassword(112233))
+	assert.False(t, isValidGroupedPassword(123444))
+	assert.True(t, isValidGroupedPassword(111122))
+}
