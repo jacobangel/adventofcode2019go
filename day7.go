2bed835c0a54cf0acf991b98d16fe7eff5f1a669
Jacob <jacob.angel@gmail.com>
2020-01-08 23:31:23 -0500

    Adding scaffolding for day 7

---
+++ day7.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func day71() {
+	fmt.Println("Day 7.1")
+	programData := LoadProgramData("./day7_input.txt")
+	fmt.Println(programData)
+	InterpretProgram(programData, 1)
+	fmt.Printf("Opcodes: %d, %d, %d, %d, %d\n", ADD, MULT, STORE, GET, STOP)
+	fmt.Printf("The answer is: '%d'\n", 42)
+}
+
+func day72() {
+	fmt.Println("Day 7.2")
+	programData := LoadProgramData("./day7_input.txt")
+	fmt.Println(programData)
+	InterpretProgram(programData, 7)
+	fmt.Printf("Opcodes: %d, %d, %d, %d, %d\n", ADD, MULT, STORE, GET, STOP)
+	fmt.Printf("The answer is: '%d'\n", 42)
+}
+
+func day7() {
+	fmt.Println("Day 7")
+	fmt.Println("---------")
+	day71()
+	day72()
+	fmt.Println()
+}
---
+++ day7_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsDay7(t *testing.T) {
+	assert.False(t, false)
+}
