ea9713602bc89098fca08eb0a8c0a874be7ced75
Jacob <jacob.angel@gmail.com>
2020-01-14 23:54:09 -0500

    Not sure there was anything to really test...

--- day8.go
+++ day8.go
@@ -2,19 +2,67 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
+/**
+The image you received is 25 pixels wide and 6 pixels tall.
+
+To make sure the image wasn't corrupted during transmission,
+the Elves would like you to find the layer that contains the fewest 0 digits.
+On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
+*/
+
+func parseImage(data string, width int, height int) [][]int {
+	layers := make([][]int, len(data)/(width*height))
+
+	for i, value := range data {
+		currLayers := i / (width * height)
+		val, _ := strconv.ParseInt(string(value), 10, 8)
+		if layers[currLayers] == nil {
+			layers[currLayers] = []int{}
+		}
+		layers[currLayers] = append(layers[currLayers], int(val))
+	}
+
+	return layers
+}
+
+func findIt(layerList [][]int) int {
+	youknowIt := 0
+	leastZeros := 1000000000000000000
+	for _, layer := range layerList {
+		count0 := 0
+		count1 := 0
+		count2 := 0
+		for _, pixel := range layer {
+			switch pixel {
+			case 0:
+				count0++
+			case 1:
+				count1++
+			case 2:
+				count2++
+			}
+		}
+		if count0 < leastZeros {
+			leastZeros = count0
+			youknowIt = count1 * count2
+		}
+	}
+	return youknowIt
+}
 
 func day81() {
 	fmt.Println("Day 8.1")
-	programData := LoadProgramData("./day8_input.txt")
-	fmt.Println(programData)
+	programData := loadFile("./day8_input.txt")
+	flatLayers := parseImage(programData, 25, 6)
+	value := findIt(flatLayers)
+	fmt.Printf("The row is: %d\n", value)
 }
 
 func day82() {
 	fmt.Println("Day 8.2")
-	programData := LoadProgramData("./day8_input.txt")
-	fmt.Println(programData)
 }
 
 func day8() {
--- day8_test.go
+++ day8_test.go
@@ -9,3 +9,11 @@ import (
 func TestIsDay8(t *testing.T) {
 	assert.False(t, false)
 }
+
+func TestParseImage(t *testing.T) {
+	layers := parseImage("123456789012", 3, 2)
+	assert.Equal(t, [][]int{
+			[]int{1,2,3,4,5,6},
+			[]int{7,8,9,0,1,2},
+	}, layers)
+}
